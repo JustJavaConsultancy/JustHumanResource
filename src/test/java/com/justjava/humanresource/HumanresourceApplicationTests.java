cb9f4fd235208bffc733cd6aaa7c104ae714532f
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2026-02-08 16:33:50 +0000

    init

---
+++ src/test/java/com/justjava/humanresource/HumanresourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justjava.humanresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanresourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
