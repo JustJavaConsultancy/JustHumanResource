e2bc73ce1b06a659a9eef26a7bccea897092ba13
Just Java Consultancy <kazeem.akinrinde72@gmail.com>
2026-02-12 09:14:10 +0100

    Added Promotion module

--- src/main/java/com/justjava/humanresource/payroll/controller/PayrollTestController.java
+++ src/main/java/com/justjava/humanresource/payroll/controller/PayrollTestController.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/test/payroll")
+@RequestMapping("/test/payroll")
 @RequiredArgsConstructor
 public class PayrollTestController {
 
