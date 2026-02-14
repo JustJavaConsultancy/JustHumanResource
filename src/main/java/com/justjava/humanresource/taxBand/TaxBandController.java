b5ad0940cd472bee885df58cc2d1dd442e7a3dd8
Akinkunmi <akinrindeakinkunmi2006@gmail.com>
2026-02-14 11:02:49 +0100

    Enhance tax band management with scrollable modal, form validation, and random regime code generation

--- src/main/java/com/justjava/humanresource/taxBand/TaxBandController.java
+++ src/main/java/com/justjava/humanresource/taxBand/TaxBandController.java
@@ -1,15 +1,38 @@
 package com.justjava.humanresource.taxBand;
 
+import com.justjava.humanresource.payroll.service.PayrollSetupService;
+import com.justjava.humanresource.payroll.statutory.entity.PayeTaxBand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class TaxBandController {
+    @Autowired
+    private PayrollSetupService payrollSetupService;
+
     @GetMapping("/tax-band")
     public String taxBand(Model model){
+
+        LocalDate date = LocalDate.now();
+
+        System.out.println("Active bands " + payrollSetupService.getActivePayeBands(date));
+
         model.addAttribute("title", "Tax Band");
         model.addAttribute("subTitle", "Manage tax bands for employees");
         return "taxBand/main";
+    }
+
+    @PostMapping("/addTaxBand")
+    public String addTaxBand(PayeTaxBand band){
+        // Logic to add a new tax band
+        System.out.println("Adding Tax Band");
+        payrollSetupService.createPayeTaxBand(band);
+        System.out.println("Tax Band added successfully: " + band.getRegimeCode());
+        return "redirect:/tax-band";
     }
 }
