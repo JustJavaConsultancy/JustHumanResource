d5e64b07500ee8606cd80b6e5a1aac069646a62c
Akinkunmi <akinrindeakinkunmi2006@gmail.com>
2026-02-17 14:19:14 +0100

    Enhance KPI and Payroll management by adding functionality to retrieve all KPI definitions, updating deduction handling in the payroll setup, and implementing modals for toggling payroll status.

--- src/main/java/com/justjava/humanresource/kpi/service/KpiDefinitionService.java
+++ src/main/java/com/justjava/humanresource/kpi/service/KpiDefinitionService.java
@@ -5,6 +5,8 @@ import com.justjava.humanresource.kpi.repositories.KpiDefinitionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class KpiDefinitionService {
@@ -14,6 +16,9 @@ public class KpiDefinitionService {
     public KpiDefinition create(KpiDefinition kpi) {
         kpi.setActive(true);
         return repository.save(kpi);
+    }
+    public List<KpiDefinition> getAll() {
+        return repository.findAll();
     }
 }
 
