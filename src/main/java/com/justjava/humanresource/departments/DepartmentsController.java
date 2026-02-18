e0be26a71110d30ff07aa9a710cd94cd6d022bdb
Akinkunmi <akinrindeakinkunmi2006@gmail.com>
2026-02-18 20:48:37 +0100

    Enhance department management view by implementing dynamic data retrieval and adding empty state handling for improved user experience

--- src/main/java/com/justjava/humanresource/departments/DepartmentsController.java
+++ src/main/java/com/justjava/humanresource/departments/DepartmentsController.java
@@ -1,6 +1,10 @@
 package com.justjava.humanresource.departments;
 
+import com.justjava.humanresource.hr.dto.DepartmentSummaryDTO;
+import com.justjava.humanresource.hr.entity.Employee;
+import com.justjava.humanresource.hr.service.DepartmentService;
 import com.justjava.humanresource.hr.service.SetupService;
+import com.justjava.humanresource.onboarding.service.EmployeeOnboardingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,13 +12,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class DepartmentsController {
     @Autowired
     private SetupService setupService;
 
+    @Autowired
+    EmployeeOnboardingService employeeOnboardingService;
+
+    @Autowired
+    private DepartmentService departmentService;
     @GetMapping("/departments")
     public String getDepartments(Model model) {
+        List<DepartmentSummaryDTO> departments = departmentService.getDepartmentSummaries();
+        departments.forEach(
+                dept -> System.out.println("Department: " + dept.getDepartmentId())
+        );
+        List<Employee> employees = employeeOnboardingService.getAllOnboardings();
+        model.addAttribute("employees", employees.size());
+        model.addAttribute("departments", departments);
+        model.addAttribute("totalDepartments", departments.size());
         model.addAttribute("title","Department Management");
         model.addAttribute("subTitle","Organize and manage company departments, teams, and hierarchies");
         return "departments/main";
