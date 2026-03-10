a4b400cc2a207aff2d90a9c7ef94fee8b0f794de
Just Java Consultancy <kazeem.akinrinde72@gmail.com>
2026-03-10 11:20:21 +0100

    Worked on ExceptionHandling and User-friendly reporting

--- src/main/java/com/justjava/humanresource/core/config/GlobalControllerAdvice.java
+++ src/main/java/com/justjava/humanresource/core/config/GlobalControllerAdvice.java
@@ -2,12 +2,16 @@ package com.justjava.humanresource.core.config;
 
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalControllerAdvice {
     @Autowired
@@ -20,5 +24,17 @@ public class GlobalControllerAdvice {
     @ModelAttribute("userName")
     public String addUserName(HttpServletRequest request) {
             return (String) authenticationManager.get("name");
+
+    }
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, Model model) {
+
+        log.error("Unhandled exception occurred", ex);
+       System.out.println(" The Error is global====="+ ex.getMessage());
+
+        model.addAttribute("errorMessage",
+                ex.getMessage());
+
+        return "error/500";
     }
 }
