27c809d2a1296be618a192aa2df766144c3f7955
Alao046 <alaoqud77@gmail.com>
2026-05-01 16:19:15 +0100

    payitems sort by id

--- src/main/java/com/justjava/humanresource/payroll/repositories/TaxReliefRepository.java
+++ src/main/java/com/justjava/humanresource/payroll/repositories/TaxReliefRepository.java
@@ -1,11 +1,11 @@
 package com.justjava.humanresource.payroll.repositories;
 
 import com.justjava.humanresource.payroll.entity.TaxRelief;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TaxReliefRepository extends JpaRepository<TaxRelief, Long> {
-
-    List<TaxRelief> findByActiveTrue();
+    List<TaxRelief> findByActiveTrue(Sort sort);
 }
